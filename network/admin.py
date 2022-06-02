fadb800f824daee31fa1ec1ac36ed3d97570d59c
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2022-06-02 14:20:03 +0700

    initial model update

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Post, FollowPair
+
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(FollowPair)
--- network/models.py
+++ network/models.py
@@ -3,4 +3,20 @@ from django.db import models
 
 
 class User(AbstractUser):
+    id = models.AutoField(primary_key=True)
     pass
+
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post")
+    content = models.CharField(max_length=512)
+    time = models.DateTimeField(auto_now=True)
+    like = models.IntegerField(default=0)
+    
+
+class FollowPair(models.Model):
+    id = models.AutoField(primary_key=True)
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+
